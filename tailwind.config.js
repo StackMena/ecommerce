fb328a3f9e3763d8a625a58e288673f18356db8a
StackMena <mena2gold@gmail.com>
2024-05-08 02:02:59 -1200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#D95129",
+        "primary-color": "#FFFFFF",
+        "secondary": "#1CA78B", 
+        "secondary-color": '#FFFFFF',
+        "gray": "#1F2937",
+        "gray-color": "#FFFFFF",
+      }
+    },
+  },
+  plugins: [],
+}
+
